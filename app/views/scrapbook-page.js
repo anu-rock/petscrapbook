9e829dfd32672ed4da30a11cd75372242010d2d3
Bhandari, Anurag X <anurag.x.bhandari@accenture.com>
2018-09-12 12:26:46 +0530

    Completed Chapter 8.

--- app/views/scrapbook-page.js
+++ app/views/scrapbook-page.js
@@ -1,13 +1,49 @@
-var observableModule = require("data/observable");
+var observable = require("data/observable");
+var observableArray = require("data/observable-array");
+var frame = require("ui/frame");
 
-exports.onLoaded = function(args){
-    var page = args.object;
-    var scrapbook = new observableModule.fromObject({
-        genders: ["Female", "Male", "Other"],
-        gender: null,
-        date: null,
-        title: null
-    });
-    
-    page.bindingContext = scrapbook;
+function scrapbookPageModel() {
+    var model = new observable.Observable();
+    model.genders = ["Female", "Male", "Other"];
+    model.calcAge = function(date) {
+        var dob = new Date(date);
+        var now = Date.now();
+        var diff = Math.abs(now - date) / 1000 / 31536000;
+        console.log(diff);
+        return diff.toFixed(1);
+    };
+    return model;
 }
+
+exports.onLoaded = function(args) {
+    var page = args.object;
+    var scrapbook;
+    if (page.navigationContext != null) {
+        scrapbook = page.navigationContext.model;
+    }
+    else {
+        scrapbook = new observable.fromObject({
+            pages: new observableArray.ObservableArray(new scrapbookPageModel())
+        });
+    }
+    page.bindingContext = scrapbook;
+};
+
+exports.onAddTap = function(args) {
+    var page = args.object;
+    var scrapbook = page.bindingContext;
+    scrapbook.pages.push(new scrapbookPageModel());
+    frame.topmost().navigate({
+        moduleName: "views/scrapbookUpdate-page",
+        context: { model: scrapbook, index: scrapbook.pages.length - 1 }
+    });
+};
+
+exports.onItemTap = function(args) {
+    var page = args.object;
+    var scrapbook = page.bindingContext;
+    frame.topmost().navigate({
+        moduleName: "views/scrapbookUpdate-page",
+        context: { model: scrapbook, index: args.index }
+    });
+};
---
+++ app/views/scrapbookUpdate-page.js
@@ -0,0 +1,17 @@
+var frame = require("ui/frame");
+
+var scrapbook;
+
+exports.onLoaded = function(args) {
+    var page = args.object;
+    var index = page.navigationContext.index;
+    scrapbook = page.navigationContext.model
+    page.bindingContext = scrapbook.pages.getItem(index);
+};
+
+exports.onDoneTap = function(args) {
+    frame.topmost().navigate({
+        moduleName: "views/scrapbook-page",
+        context: { model: scrapbook }
+    });
+};
